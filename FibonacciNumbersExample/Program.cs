1274c538f10cd430463d634e6c5b39909673eb83
jshudd <jshudd@gmail.com>
2020-04-09 09:16:32 -0500

    added MS answer

--- FibonacciNumbersExample/Program.cs
+++ FibonacciNumbersExample/Program.cs
@@ -20,6 +20,19 @@ namespace FibonacciNumbersExample
                 fibonacciNumbers.Add(nextNum);
                 Console.WriteLine(fibonacciNumbers[i]);
             }
+
+            /*MS Answer
+             *var fibonacciNumbers = new List<int> {1, 1};
+
+              while (fibonacciNumbers.Count < 20)
+               {
+                  var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                  var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                  fibonacciNumbers.Add(previous + previous2);
+                }
+               foreach(var item in fibonacciNumbers)
+               Console.WriteLine(item);*/
         }
     }
 }
