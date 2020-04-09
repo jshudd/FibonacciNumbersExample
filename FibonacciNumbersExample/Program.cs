4319ce2a969d04ebb8945e066747d7dc42ea19c1
jshudd <jshudd@gmail.com>
2020-04-09 09:08:41 -0500

    finished exercise

---
+++ FibonacciNumbersExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciNumbersExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            Console.WriteLine(fibonacciNumbers[0]);
+            Console.WriteLine(fibonacciNumbers[1]);
+
+            for (var i = 2; i < 20; i++)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                var nextNum = previous + previous2;
+                fibonacciNumbers.Add(nextNum);
+                Console.WriteLine(fibonacciNumbers[i]);
+            }
+        }
+    }
+}
